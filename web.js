1dee2fe75ba8838b58d42d7d9a31bb0cc3a40287
Ken Mickles <ken@37i.net>
2017-07-12 17:42:52 -0400

    Remove dependency on ancient Node version
    In response to
    https://nodejs.org/en/blog/vulnerability/july-2017-security-releases/

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var app = express();
 
-var app = express.createServer(express.logger());
+var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
+var port = process.env.PORT || 5000;
 
 app.get('*', function(request, response) {
-  response.redirect(process.env.NEW_BASE_URL + request.url)
+  response.redirect(newBaseURL + request.url);
 });
 
-var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
