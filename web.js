f03b50420ce2c5481db10762bb2bf41811007c5b
Arne Welzel <arne.welzel@gmail.com>
2018-04-25 19:10:23 +0200

    Support configuration of HTTP status code

--- web.js
+++ web.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 
 var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
+var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
 var port = process.env.PORT || 5000;
 
 app.get('*', function(request, response) {
-  response.redirect(newBaseURL + request.url);
+  response.redirect(redirectStatus, newBaseURL + request.url);
 });
 
 app.listen(port, function() {
