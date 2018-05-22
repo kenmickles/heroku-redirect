645e9a2efb7cb18aa86e01a1dc0f4891db4df740
Kevin Haight <khaight@brooksbell.com>
2018-05-21 21:35:51 -0400

    added wildcard support

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var express = require('express');
+var subdomain = require('express-subdomain');
 var app = express();
 
-var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
+var newBaseURL = process.env.NEW_BASE_URL || 'http://www.example.com';
 var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
 var port = process.env.PORT || 5000;
 
 app.get('*', function(request, response) {
-  response.redirect(redirectStatus, newBaseURL + request.url);
+  var baseURL = newBaseURL.replace('*', request.subdomains[0]);
+  response.redirect(redirectStatus, baseURL + request.url);
 });
 
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
