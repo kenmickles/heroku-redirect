471365f3c31de71ad418bc4355dcfd3997beaeb3
uivatury <uday@bridgebase.com>
2019-06-09 13:34:33 -0400

    Route to web4, web0, or home page

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var app = express();
 
-var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
+var newDefaultURL = process.env.NEW_DEFAULT_URL || 'https://www.bridgebase.com/';
+var newWeb4URL = process.env.NEW_WEB4_URL || 'https://www.bridgebase.com/web4.php?cb=web4obsdef';
+var newWeb0URL = process.env.NEW_WEB0_URL || 'https://www.bridgebase.com/web0.php?cb=web0obsdef';
+
 var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
 var port = process.env.PORT || 5000;
 
 app.get('*', function(request, response) {
-  response.redirect(redirectStatus, newBaseURL + request.url);
+  if ( request.url ) {
+    if ( request.url.includes('/web4')  ) {
+      response.redirect(redirectStatus, newWeb4URL);  
+    } else {
+      response.redirect(redirectStatus, newWeb0URL);  
+    }
+  } else {
+    response.redirect(redirectStatus, newDefaultURL);
+  }
 });
 
 app.listen(port, function() {
